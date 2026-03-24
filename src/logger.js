5c259c9f0534509b6629720bef66944db8f84b08
ammonwk <ammonwk@gmail.com>
2026-03-24 02:07:49 -0600

    Add logger module with core Grafana Loki infrastructure

---
+++ src/logger.js
@@ -0,0 +1,44 @@
+const config = require('./config.js');
+
+class Logger {
+  log(level, type, logData) {
+    const labels = { component: config.logging.source, level: level, type: type };
+    const values = [this.nowString(), this.sanitize(logData)];
+    const logEvent = { streams: [{ stream: labels, values: [values] }] };
+
+    this.sendLogToGrafana(logEvent);
+  }
+
+  statusToLogLevel(statusCode) {
+    if (statusCode >= 500) return 'error';
+    if (statusCode >= 400) return 'warn';
+    return 'info';
+  }
+
+  nowString() {
+    return (Math.floor(Date.now()) * 1000000).toString();
+  }
+
+  sanitize(logData) {
+    logData = JSON.stringify(logData);
+    logData = logData.replace(/\\"password\\":\s*\\"[^"]*\\"/g, '\\"password\\": \\"*****\\"');
+    logData = logData.replace(/\\"token\\":\s*\\"[^"]*\\"/g, '\\"token\\": \\"*****\\"');
+    return logData;
+  }
+
+  sendLogToGrafana(event) {
+    const body = JSON.stringify(event);
+    fetch(`${config.logging.endpointUrl}`, {
+      method: 'post',
+      body: body,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${config.logging.accountId}:${config.logging.apiKey}`,
+      },
+    }).then((res) => {
+      if (!res.ok) console.log('Failed to send log to Grafana');
+    });
+  }
+}
+
+module.exports = new Logger();
