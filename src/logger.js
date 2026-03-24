cf243cb30a9f2df360d6b8daa54df1bf2883e153
ammonwk <ammonwk@gmail.com>
2026-03-24 02:08:16 -0600

    Add HTTP request logging middleware

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,25 @@
 const config = require('./config.js');
 
 class Logger {
+  httpLogger = (req, res, next) => {
+    let send = res.send;
+    res.send = (resBody) => {
+      const logData = {
+        authorized: !!req.headers.authorization,
+        path: req.originalUrl,
+        method: req.method,
+        statusCode: res.statusCode,
+        reqBody: JSON.stringify(req.body),
+        resBody: JSON.stringify(resBody),
+      };
+      const level = this.statusToLogLevel(res.statusCode);
+      this.log(level, 'http', logData);
+      res.send = send;
+      return res.send(resBody);
+    };
+    next();
+  };
+
   log(level, type, logData) {
     const labels = { component: config.logging.source, level: level, type: type };
     const values = [this.nowString(), this.sanitize(logData)];
